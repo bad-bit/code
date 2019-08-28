0057f33fc0d6f0b3374755c1e6e31e5424e62295
Vaibhav Choudhari <vaibhavchoudhari05@gmail.com>
2019-08-28 18:34:07 +0530

    Updated regex to include the following type of input:aaxx-xx-aa

--- regex.py
+++ regex.py
@@ -11,16 +11,19 @@ def main():
 		card_list.append(target)
 
 	for x in card_list:
-		status_start = bool(re.match(r"^[4, 5, 6]\d\d\d\-\d\d\d\d\-\d\d\d\d\-\d\d\d\d", x))
+		status_start = bool(re.match(r"^[4, 5, 6]\d{3}\-{0,1}\d{4}\-{0,1}\d{4}\-{0,1}\d{4}$", x) and not re.search(r"([\d])\1\1\1", x.replace("-", "")))
+		#more readable version of the regex:
+		#	^[4, 5, 6]\d\d\d\-{0,1}\d\d\d\d\-{0,1}\d\d\d\d\-{0,1}\d\d\d\d
 
-
-		if status_start == True:# and len(x) == 16:
+		if status_start == True:
 			print("Valid")
 		else:
 			print("Invalid")
 
-
 if __name__ == '__main__':
 	main()
 
-	#^[4, 5, 6]\d\d\d\-{0,1}\d\d\d\d\-{0,1}\d\d\d\d\-{0,1}\d\d\d\d
+	
+
+	#fail
+	#^[4, 5, 6]\d\d\d\-\d\d\d\d\-\d\d\d\d\-\d\d\d\d
